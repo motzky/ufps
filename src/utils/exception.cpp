b9918c1a2371312ea7a18d38b3d6cb1e584eded4
Christian Moyzyczyk <motzky@gmx.net>
2025-11-26 22:24:59 +0100

    Add exception implementation

---
+++ src/utils/exception.cpp
@@ -0,0 +1,23 @@
+#include "utils/exception.h"
+
+#include <format>
+#include <iostream>
+
+namespace ufps
+{
+    auto Exception::stacktrace() const -> std::string
+    {
+        return std::to_string(_trace);
+    }
+
+    auto Exception::what() const -> std::string_view
+    {
+        return _what;
+    }
+
+    auto Exception::to_string() const -> std::string
+    {
+        return std::format("{}\n{}", what(), stacktrace());
+    }
+
+}
