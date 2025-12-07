d59470fc5c28726d026574d13cf6cbd9b7df3d20
Christian Moyzyczyk <motzky@gmx.net>
2025-12-07 21:45:26 +0100

    Add MeshManager class

---
+++ include/graphics/mesh_manager.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+#include "graphics/buffer.h"
+#include "graphics/opengl.h"
+#include "graphics/vertex_data.h"
+
+namespace ufps
+{
+    struct MeshView
+    {
+        std::uint32_t offset;
+        std::uint32_t count;
+    };
+
+    class MeshManager
+    {
+    public:
+        MeshManager();
+        auto load(const std::vector<VertexData> &mesh) -> MeshView;
+
+        auto native_handle() const -> ::GLuint;
+
+        auto to_string() const -> std::string;
+
+    private:
+        std::vector<VertexData> _mesh_data_cpu;
+        Buffer _mesh_data_gpu;
+    };
+}
---
+++ src/graphics/mesh_manager.cpp
@@ -0,0 +1,57 @@
+#include "graphics/mesh_manager.h"
+
+#include <cstdint>
+#include <ranges>
+#include <vector>
+
+#include "graphics/buffer.h"
+#include "graphics/vertex_data.h"
+#include "log.h"
+#include "utils/formatter.h"
+
+namespace ufps
+{
+    MeshManager::MeshManager()
+        : _mesh_data_cpu{},
+          _mesh_data_gpu{sizeof(VertexData), "mesh_data"}
+    {
+        _mesh_data_cpu.reserve(1zu);
+    }
+
+    auto MeshManager::load(const std::vector<VertexData> &mesh) -> MeshView
+    {
+        const auto offset = _mesh_data_cpu.size();
+
+        _mesh_data_cpu.append_range(mesh);
+
+        const auto buffer_size_bytes = _mesh_data_cpu.size() * sizeof(VertexData);
+
+        if (_mesh_data_gpu.size() <= buffer_size_bytes)
+        {
+            auto new_size = _mesh_data_gpu.size() * 2;
+            while (new_size < buffer_size_bytes)
+            {
+                new_size *= 2;
+            }
+
+            log::info("growing mesh_data buffer {:x} -> {:x}", _mesh_data_gpu.size(), new_size);
+            _mesh_data_gpu = Buffer{new_size, "mesh_data"};
+        }
+
+        auto mesh_view = DataBufferView{reinterpret_cast<std::byte *>(_mesh_data_cpu.data()), buffer_size_bytes};
+        _mesh_data_gpu.write(mesh_view, 0zu);
+
+        return {.offset = static_cast<std::uint32_t>(offset), .count = static_cast<std::uint32_t>(mesh.size())};
+    }
+
+    auto MeshManager::native_handle() const -> ::GLuint
+    {
+        return _mesh_data_gpu.native_handle();
+    }
+
+    auto MeshManager::to_string() const -> std::string
+    {
+        return std::format("mesh manager: vertex count: {:x}", _mesh_data_cpu.size());
+    }
+
+}
