e5e74441aa2eb7e979b70efe37f14400d0cad795
Christian Moyzyczyk <motzky@gmx.net>
2025-12-11 23:38:57 +0100

    Add Matrix4 class and tests

---
+++ include/math/matrix4.h
@@ -0,0 +1,248 @@
+#pragma once
+
+#include <array>
+#include <cstring>
+#include <format>
+#include <ranges>
+#include <span>
+
+#include "math/vector3.h"
+#include "utils/ensure.h"
+
+namespace ufps
+{
+    class Matrix4
+    {
+    public:
+        struct Scale
+        {
+        };
+        // initialize to identity
+        constexpr Matrix4()
+            : _elements({1.f,
+                         0.f,
+                         0.f,
+                         0.f,
+                         0.f,
+                         1.f,
+                         0.f,
+                         0.f,
+                         0.f,
+                         0.f,
+                         1.f,
+                         0.f,
+                         0.f,
+                         0.f,
+                         0.f,
+                         1.f})
+        {
+        }
+
+        constexpr explicit Matrix4(const std::array<float, 16u> &elements)
+            : Matrix4{std::span<const float>{elements}}
+        {
+        }
+
+        constexpr explicit Matrix4(const std::span<const float> &elements)
+            : Matrix4{}
+        {
+            ensure(elements.size() == 16u, "not enough elements");
+            std::ranges::copy(elements, std::ranges::begin(_elements));
+            // std::memcpy(_elements.data(), elements.data(), elements.size_bytes());
+        }
+
+        constexpr Matrix4(const Vector3 &translation)
+            : _elements({1.f,
+                         0.f,
+                         0.f,
+                         0.f,
+                         0.f,
+                         1.f,
+                         0.f,
+                         0.f,
+                         0.f,
+                         0.f,
+                         1.f,
+                         0.f,
+                         translation.x,
+                         translation.y,
+                         translation.z,
+                         1.f})
+        {
+        }
+
+        constexpr Matrix4(const Vector3 &translation, const Vector3 &scale)
+            : _elements({scale.x,
+                         0.f,
+                         0.f,
+                         0.f,
+                         0.f,
+                         scale.y,
+                         0.f,
+                         0.f,
+                         0.f,
+                         0.f,
+                         scale.z,
+                         0.f,
+                         translation.x,
+                         translation.y,
+                         translation.z,
+                         1.f})
+        {
+        }
+
+        constexpr Matrix4(const Vector3 &scale, Scale)
+            : _elements({scale.x,
+                         0.f,
+                         0.f,
+                         0.f,
+                         0.f,
+                         scale.y,
+                         0.f,
+                         0.f,
+                         0.f,
+                         0.f,
+                         scale.z,
+                         0.f,
+                         0.f,
+                         0.f,
+                         0.f,
+                         1.f})
+        {
+        }
+
+        static constexpr auto look_at(const Vector3 &eye, const Vector3 &look_at, const Vector3 &up) -> Matrix4;
+        static constexpr auto perspective(float fov, float width, float height, float near_plane, float far_plane) -> Matrix4;
+        static constexpr auto orthographic(float width, float height, float depth) -> Matrix4;
+
+        auto make_translate(const ufps::Vector3 &v1) -> ufps::Matrix4
+        {
+            return {v1};
+        }
+
+        constexpr auto data() const -> std::span<const float>
+        {
+            return _elements;
+        }
+
+        auto operator[](this auto &&self, std::size_t index) -> auto
+        {
+            return self._elements[index];
+        }
+
+        // auto row(std::size_t index) const -> Vector4
+        // {
+        //     expect(index < 4, "index out of range");
+
+        //     return {_elements[index], _elements[index + 4u], _elements[index + 8u], _elements[index + 12u]};
+        // }
+
+        friend constexpr auto operator*=(Matrix4 &m1, const Matrix4 &m2) -> Matrix4 &;
+
+        constexpr auto operator==(const Matrix4 &) const -> bool = default;
+
+        auto to_string() const -> std::string;
+
+    private:
+        std::array<float, 16u> _elements;
+    };
+
+    constexpr auto operator*=(Matrix4 &m1, const Matrix4 &m2) -> Matrix4 &
+    {
+        auto result = Matrix4{};
+        for (auto i = 0u; i < 4u; i++)
+        {
+            for (auto j = 0u; j < 4u; j++)
+            {
+                auto sum = 0.f;
+                for (auto k = 0u; k < 4u; ++k)
+                {
+                    sum += m1._elements[i + k * 4] * m2._elements[k + j * 4];
+                }
+                result._elements[i + j * 4] = sum;
+            }
+        }
+
+        m1 = result;
+        return m1;
+    }
+
+    constexpr auto operator*(const Matrix4 &m1, const Matrix4 &m2) -> Matrix4
+    {
+        auto tmp{m1};
+        return tmp *= m2;
+    }
+
+    // constexpr auto operator*(const Matrix4 &m, const Vector4 &v) -> Vector4
+    // {
+    //     return {
+    //         m[0] * v.x + m[4] * v.y + m[8] * v.z + m[12] * v.w,
+    //         m[1] * v.x + m[5] * v.y + m[9] * v.z + m[13] * v.w,
+    //         m[2] * v.x + m[6] * v.y + m[10] * v.z + m[14] * v.w,
+    //         m[3] * v.x + m[7] * v.y + m[11] * v.z + m[15] * v.w,
+    //     };
+    // }
+
+    inline constexpr auto Matrix4::look_at(const Vector3 &eye, const Vector3 &look_at, const Vector3 &up) -> Matrix4
+    {
+        const auto f = Vector3::normalize(look_at - eye);
+        const auto up_normalized = Vector3::normalize(up);
+
+        const auto s = Vector3::normalize(Vector3::cross(f, up_normalized));
+        const auto u = Vector3::normalize(Vector3::cross(s, f));
+
+        auto m = Matrix4{};
+
+        m._elements = {s.x, u.x, -f.x, 0.f,
+                       s.y, u.y, -f.y, 0.f,
+                       s.z, u.z, -f.z, 0.f,
+                       0.f, 0.f, 0.f, 1.f};
+
+        return m * Matrix4{-eye};
+    }
+
+    inline constexpr auto Matrix4::perspective(float fov, float width, float height, float near_plane, float far_plane) -> Matrix4
+    {
+        Matrix4 m;
+
+        const auto aspect_ratio = width / height;
+        const auto tmp = std::tanf(fov / 2.f);
+        const auto t = tmp * near_plane;
+        const auto b = -t;
+        const auto r = t * aspect_ratio;
+        const auto l = b * aspect_ratio;
+
+        m._elements = {{(2.f * near_plane) / (r - l), 0.f, 0.f, 0.f,
+                        0.f, (2.f * near_plane) / (t - b), 0.f, 0.f,
+                        (r + l) / (r - l), (t + b) / (t - b), -(far_plane + near_plane) / (far_plane - near_plane), -1.f,
+                        0.f, 0.f, -(2.f * far_plane * near_plane) / (far_plane - near_plane), 0.f}};
+
+        return m;
+    }
+
+    inline constexpr auto Matrix4::orthographic(float width, float height, float depth) -> Matrix4
+    {
+        const auto right = width / 2.f;
+        const auto left = -right;
+        const auto top = height / 2.f;
+        const auto bottom = -top;
+        const auto far_p = depth;
+        const auto near_p = 0.0f;
+
+        return Matrix4{{2.f / (right - left), 0.f, 0.f, 0.f,
+                        0.f, 2.f / (top - bottom), 0.f, 0.f,
+                        0.f, 0.f, -2.f / (far_p - near_p), 0.f,
+                        -(right + left) / (right - left), -(top + bottom) / (top - bottom), -(far_p + near_p) / (far_p - near_p), 1.f}};
+    }
+
+    inline auto Matrix4::to_string() const -> std::string
+    {
+        const auto *d = data().data();
+        return std::format("{} {} {} {}\n{} {} {} {}\n{} {} {} {}\n{} {} {} {}",
+                           d[0], d[4], d[8], d[12],
+                           d[1], d[5], d[9], d[13],
+                           d[2], d[6], d[10], d[14],
+                           d[3], d[7], d[11], d[15]);
+    }
+
+}
---
+++ tests/matrix4_tests.cpp
@@ -0,0 +1,193 @@
+#include <gtest/gtest.h>
+
+#include <cstring>
+#include <numbers>
+#include <print>
+
+#include "math/matrix4.h"
+#include "math/vector3.h"
+
+TEST(matrix4, identity_ctor)
+{
+    const auto m = ufps::Matrix4{};
+    const auto expected = std::array<float, 16u>{
+        1.f,
+        0.f,
+        0.f,
+        0.f,
+        0.f,
+        1.f,
+        0.f,
+        0.f,
+        0.f,
+        0.f,
+        1.f,
+        0.f,
+        0.f,
+        0.f,
+        0.f,
+        1.f,
+    };
+
+    const auto result = m.data();
+    ASSERT_EQ(result.size(), expected.size());
+    EXPECT_EQ(std::memcmp(result.data(), expected.data(), result.size_bytes()), 0);
+}
+
+TEST(matrix4, element_ctor)
+{
+    const auto expected = std::array<float, 16u>{
+        1.f,
+        2.f,
+        3.f,
+        4.f,
+        5.f,
+        6.f,
+        7.f,
+        8.f,
+        9.f,
+        10.f,
+        11.f,
+        12.f,
+        13.f,
+        14.f,
+        15.f,
+        16.f,
+    };
+
+    const auto m = ufps::Matrix4{expected};
+
+    const auto result = m.data();
+    ASSERT_EQ(result.size(), expected.size());
+    EXPECT_EQ(std::memcmp(result.data(), expected.data(), result.size_bytes()), 0);
+}
+
+TEST(matrix4, incorrect_element_size_ctor)
+{
+    const auto expected = std::array<float, 161u>{
+        1.f,
+    };
+
+    EXPECT_THROW(ufps::Matrix4{expected}, ufps::Exception);
+}
+
+TEST(matrix4, equals_operator)
+{
+    const auto m1 = ufps::Matrix4{};
+
+    ASSERT_TRUE(m1 == m1);
+}
+
+TEST(matrix4, multiply_by_identity)
+{
+    const auto m1 = ufps::Matrix4{{
+        1.f,
+        2.f,
+        3.f,
+        4.f,
+        1.f,
+        2.f,
+        3.f,
+        4.f,
+        1.f,
+        2.f,
+        3.f,
+        4.f,
+        1.f,
+        2.f,
+        3.f,
+        4.f,
+    }};
+    const auto m2 = ufps::Matrix4{};
+
+    ASSERT_EQ(m1 * m2, m1);
+}
+
+TEST(matrix4, multiply)
+{
+    const auto m1 = ufps::Matrix4{{1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f, 9.f, 10.f,
+                                   11.f, 12.f, 13.f, 14.f, 15.f, 16.f}};
+
+    const auto m2 = ufps::Matrix4{{1.5f, 2.5f, 3.5f, 4.5f, 5.5f, 6.5f, 7.5f, 8.5f, 9.5f, 10.5f,
+                                   11.5f, 12.5f, 13.5f, 14.5f, 15.5f, 16.5f}};
+
+    const auto expected = ufps::Matrix4{{104.f, 116.f, 128.f, 140.f, 216.f, 244.f, 272.f, 300.f, 328.f, 372.f, 416.f, 460.f, 440.f, 500.f, 560.f, 620.f}};
+
+    ASSERT_EQ(m1 * m2, expected);
+}
+TEST(matrix4, multiply_assing)
+{
+    auto m = ufps::Matrix4{{1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f, 9.f, 10.f,
+                            11.f, 12.f, 13.f, 14.f, 15.f, 16.f}};
+
+    m *= ufps::Matrix4{{1.5f, 2.5f, 3.5f, 4.5f, 5.5f, 6.5f, 7.5f, 8.5f, 9.5f, 10.5f,
+                        11.5f, 12.5f, 13.5f, 14.5f, 15.5f, 16.5f}};
+
+    const auto expected = ufps::Matrix4{{104.f, 116.f, 128.f, 140.f, 216.f, 244.f, 272.f, 300.f, 328.f, 372.f, 416.f, 460.f, 440.f, 500.f, 560.f, 620.f}};
+
+    ASSERT_EQ(m, expected);
+}
+
+TEST(matrix4, look_at)
+{
+    const auto view = ufps::Matrix4::look_at({1.f, 0.f, 5.f}, {0.f}, {0.f, 1.f, 0.f});
+    float vals[] = {0.980581f, 0.f, 0.196116f, 0.f,
+                    0.f, 1.f, 0.f, 0.f,
+                    -0.196116f, 0.f, 0.980581f, 0.f,
+                    0.f, 0.f, -5.099020f, 1.f};
+    const auto expected = ufps::Matrix4{vals};
+
+    const auto view_spn = view.data();
+    const auto exp_spn = expected.data();
+    for (auto i = 0u; i < 16u; i++)
+    {
+        ASSERT_NEAR(view_spn[i], exp_spn[i], 0.00001f);
+    }
+}
+
+TEST(matrix4, perspective)
+{
+    const auto proj = ufps::Matrix4::perspective(std::numbers::pi_v<float> / 4.f, 800.f, 600.f, 0.1f, 100.f);
+    float vals[] = {1.81066f, 0.f, 0.f, 0.f,
+                    0.f, 2.414213f, 0.f, 0.f,
+                    0.f, 0.f, -1.002002f, -1.f,
+                    0.f, 0.f, -0.200200f, 0.f};
+    const auto expected = ufps::Matrix4{vals};
+
+    const auto view_spn = proj.data();
+    const auto exp_spn = expected.data();
+    for (auto i = 0u; i < 16u; i++)
+    {
+        ASSERT_NEAR(view_spn[i], exp_spn[i], 0.00001f);
+    }
+}
+
+// TEST(matrix4, mul_vector4)
+// {
+//     const auto m = ufps::Matrix4{
+//         {1.f,
+//          2.f,
+//          3.f,
+//          4.f,
+//          5.f,
+//          6.f,
+//          7.f,
+//          8.f,
+//          9.f,
+//          10.f,
+//          11.f,
+//          12.f,
+//          13.f,
+//          14.f,
+//          15.f,
+//          16.f}};
+
+//     const auto v = ufps::Vector4{.5f, 1.5f, 2.5f, 3.5f};
+
+//     const auto result = m * v;
+//     std::println("{}", result);
+
+//     const auto expected = ufps::Vector4{76.f, 84.f, 92.f, 100.f};
+
+//     ASSERT_EQ(result, expected);
+// }
