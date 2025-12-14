7714943078cb33c2afe3bbc5349cdff1b2404d59
Christian Moyzyczyk <motzky@gmx.net>
2025-12-14 22:00:20 +0100

    Add Transform to Entity

--- include/graphics/entity.h
+++ include/graphics/entity.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #include "graphics/mesh_view.h"
+#include "math/transform.h"
 
 namespace ufps
 {
     struct Entity
     {
         MeshView mesh_view;
+        Transform transform;
     };
 }
