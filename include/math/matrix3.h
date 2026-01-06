ba029a065de080dcea199cb93bdae9d97c89cab6
Christian Moyzyczyk <motzky@gmx.net>
2026-01-06 15:46:34 +0100

    Add Matrix3 class and tests

---
+++ include/math/matrix3.h
@@ -0,0 +1,169 @@
+#pragma once
+
+#include <array>
+#include <cstring>
+#include <format>
+#include <ranges>
+#include <span>
+
+#include "math/quaternion.h"
+#include "math/vector3.h"
+#include "math/vector4.h"
+#include "utils/ensure.h"
+
+namespace ufps
+{
+    class Matrix3
+    {
+    public:
+        struct Scale
+        {
+        };
+        // initialize to identity
+        constexpr Matrix3()
+            : _elements({1.f,
+                         0.f,
+                         0.f,
+                         0.f,
+                         1.f,
+                         0.f,
+                         0.f,
+                         0.f,
+                         1.f})
+        {
+        }
+
+        constexpr Matrix3(const std::array<float, 9u> &elements)
+            : _elements{elements}
+        {
+        }
+
+        constexpr Matrix3(const Vector3 &v1, const Vector3 &v2, const Vector3 &v3)
+            : _elements(
+                  {v1.x, v1.y, v1.z,
+                   v2.x, v2.y, v2.z,
+                   v3.x, v3.y, v3.z})
+        {
+        }
+
+        Matrix3(const std::span<const float> &elements)
+            : Matrix3{}
+        {
+            ensure(elements.size() == 9u, "not enough elements");
+            std::memcpy(_elements.data(), elements.data(), elements.size_bytes());
+        }
+
+        constexpr auto data() const -> std::span<const float>
+        {
+            return _elements;
+        }
+
+        constexpr auto operator[](this auto &&self, std::size_t index) -> auto &
+        {
+            return self._elements[index];
+        }
+
+        static constexpr auto transpose(Matrix3 m) -> Matrix3
+        {
+            std::ranges::swap(m[1], m[3]);
+            std::ranges::swap(m[2], m[6]);
+            std::ranges::swap(m[5], m[7]);
+
+            return m;
+        }
+
+        auto row(std::size_t index) const -> Vector3
+        {
+            ensure(index < 3, "index out of range");
+
+            return {_elements[index], _elements[index + 3u], _elements[index + 6u]};
+        }
+
+        friend constexpr auto operator*=(Matrix3 &m1, const Matrix3 &m2) -> Matrix3 &;
+
+        constexpr auto operator==(const Matrix3 &) const -> bool = default;
+
+        static constexpr auto determinate(const Matrix3 &m) -> float
+        {
+            return m[0] * m[4] * m[8] +
+                   m[1] * m[5] * m[6] +
+                   m[2] * m[3] * m[7] -
+
+                   m[0] * m[5] * m[7] -
+                   m[1] * m[3] * m[8] -
+                   m[2] * m[4] * m[6];
+        }
+
+        static constexpr auto invert(const Matrix3 &m) -> Matrix3
+        {
+            const auto adjoint = Matrix3{{(m[4] * m[8]) - (m[5] * m[7]),
+                                          -(m[1] * m[8]) + (m[2] * m[7]),
+                                          (m[1] * m[5]) - (m[2] * m[4]),
+
+                                          -(m[3] * m[8]) + (m[5] * m[6]),
+                                          (m[0] * m[8]) - (m[2] * m[6]),
+                                          -(m[0] * m[5]) + (m[2] * m[3]),
+
+                                          (m[3] * m[7]) - (m[4] * m[6]),
+                                          -(m[0] * m[7]) + (m[1] * m[6]),
+                                          (m[0] * m[4]) - (m[1] * m[3])}};
+
+            const auto det = determinate(m);
+
+            const auto inv_vals = adjoint.data() | std::views::transform([det](auto e)
+                                                                         { return e / det; });
+            auto inv_arr = std::array<float, 9u>{};
+            std::ranges::copy(inv_vals, std::ranges::begin(inv_arr));
+
+            return {inv_arr};
+        }
+
+        auto to_string() const -> std::string;
+
+    private:
+        std::array<float, 9u>
+            _elements;
+    };
+
+    constexpr auto operator*=(Matrix3 &m1, const Matrix3 &m2) -> Matrix3 &
+    {
+        auto result = Matrix3{};
+        for (auto i = 0u; i < 3u; i++)
+        {
+            for (auto j = 0u; j < 3u; j++)
+            {
+                auto sum = 0.f;
+                for (auto k = 0u; k < 3u; ++k)
+                {
+                    sum += m1._elements[i + k * 3] * m2._elements[k + j * 3];
+                }
+                result._elements[i + j * 3] = sum;
+            }
+        }
+
+        m1 = result;
+        return m1;
+    }
+
+    constexpr auto operator*(const Matrix3 &m1, const Matrix3 &m2) -> Matrix3
+    {
+        auto tmp{m1};
+        return tmp *= m2;
+    }
+
+    constexpr auto operator*(const Matrix3 &m, const Vector3 &v) -> Vector3
+    {
+        return {m[0] * v.x + m[3] * v.y + m[6] * v.z,
+                m[1] * v.x + m[4] * v.y + m[7] * v.z,
+                m[2] * v.x + m[5] * v.y + m[8] * v.z};
+    }
+
+    inline auto Matrix3::to_string() const -> std::string
+    {
+        const auto *d = data().data();
+        return std::format("{} {} {}\n{} {} {}\n{} {} {}",
+                           d[0], d[3], d[6],
+                           d[1], d[4], d[7],
+                           d[2], d[5], d[8]);
+    }
+}
---
+++ tests/matrix3_tests.cpp
@@ -0,0 +1,167 @@
+#include <gtest/gtest.h>
+
+#include <numbers>
+#include <print>
+
+#include "math/matrix3.h"
+#include "math/vector3.h"
+
+TEST(matrix3, ctor_identity)
+{
+    float vals[] = {
+        1.f,
+        0.f,
+        0.f,
+        0.f,
+        1.f,
+        0.f,
+        0.f,
+        0.f,
+        1.f,
+    };
+    const auto m = ufps::Matrix3{};
+
+    ASSERT_FLOAT_EQ(m[0], vals[0]);
+    ASSERT_FLOAT_EQ(m[1], vals[1]);
+    ASSERT_FLOAT_EQ(m[2], vals[2]);
+    ASSERT_FLOAT_EQ(m[3], vals[3]);
+    ASSERT_FLOAT_EQ(m[4], vals[4]);
+    ASSERT_FLOAT_EQ(m[5], vals[5]);
+    ASSERT_FLOAT_EQ(m[6], vals[6]);
+    ASSERT_FLOAT_EQ(m[7], vals[7]);
+    ASSERT_FLOAT_EQ(m[8], vals[8]);
+}
+
+TEST(matrix3, ctor_from_three_vectors)
+{
+    const auto v1 = ufps::Vector3{3.f, 2.f, 1.f};
+    const auto v2 = ufps::Vector3{7.f, 8.f, 9.f};
+    const auto v3 = ufps::Vector3{4.f, 5.f, 6.f};
+
+    const auto m = ufps::Matrix3(v1, v2, v3);
+
+    ASSERT_FLOAT_EQ(m[0], 3.f);
+    ASSERT_FLOAT_EQ(m[1], 2.f);
+    ASSERT_FLOAT_EQ(m[2], 1.f);
+    ASSERT_FLOAT_EQ(m[3], 7.f);
+    ASSERT_FLOAT_EQ(m[4], 8.f);
+    ASSERT_FLOAT_EQ(m[5], 9.f);
+    ASSERT_FLOAT_EQ(m[6], 4.f);
+    ASSERT_FLOAT_EQ(m[7], 5.f);
+    ASSERT_FLOAT_EQ(m[8], 6.f);
+}
+
+TEST(matrix3, multiply_by_identity)
+{
+    float vals[] = {
+        1.f,
+        2.f,
+        3.f,
+        1.f,
+        2.f,
+        3.f,
+        1.f,
+        2.f,
+        3.f,
+    };
+    const auto m1 = ufps::Matrix3{vals};
+    const auto m2 = ufps::Matrix3{};
+
+    ASSERT_EQ(m1 * m2, m1);
+}
+
+TEST(matrix3, multiply_by_matrix)
+{
+    const auto m1 = ufps::Matrix3{{0.5f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f}};
+    const auto m2 = ufps::Matrix3{{0.5f, 1.5f, 2.5f, 3.5f, 4.5f, 5.5f, 6.5f, 7.5f, 8.5f}};
+
+    const auto expected = ufps::Matrix3{
+        {19.75f, 24.f, 28.5f, 48.25f, 60.f, 73.5f, 76.75f, 96.f, 118.5f}};
+
+    ASSERT_EQ(m1 * m2, expected);
+}
+
+TEST(matrix3, multiply_with_vector)
+{
+    const auto v1 = ufps::Vector3{1.f, 2.f, 3.f};
+    const auto v2 = ufps::Vector3{4.f, 5.f, 6.f};
+    const auto v3 = ufps::Vector3{7.f, 8.f, 9.f};
+
+    const auto m = ufps::Matrix3(v1, v2, v3);
+    const auto v4 = ufps::Vector3{10.f, 10.f, 10.f};
+
+    const auto mult = m * v4;
+
+    ASSERT_FLOAT_EQ(mult.x, 120.f);
+    ASSERT_FLOAT_EQ(mult.y, 150.f);
+    ASSERT_FLOAT_EQ(mult.z, 180.f);
+}
+
+TEST(matrix3, determinate)
+{
+    float vals[] = {
+        1.f, 2.f, -1.f,
+        2.f, 1.f, 2.f,
+        -1.f, 2.f, 1.f};
+
+    float expected = -16.f;
+
+    const auto m = ufps::Matrix3{vals};
+
+    const auto det = ufps::Matrix3::determinate(m);
+
+    ASSERT_FLOAT_EQ(det, expected);
+}
+
+TEST(matrix3, invert)
+{
+    float vals[] = {
+        1.f, 2.f, -1.f,
+        2.f, 1.f, 2.f,
+        -1.f, 2.f, 1.f};
+
+    float expected_vals[] = {
+        3.f / 16.f, .25f, -5.f / 16.f,
+        .25f, 0.f, .25f,
+        -5.f / 16.f, .25f, 3.f / 16.f};
+
+    const auto m = ufps::Matrix3{vals};
+
+    const auto m_inv = ufps::Matrix3::invert(m);
+
+    ASSERT_FLOAT_EQ(m_inv[0], expected_vals[0]);
+    ASSERT_FLOAT_EQ(m_inv[1], expected_vals[1]);
+    ASSERT_FLOAT_EQ(m_inv[2], expected_vals[2]);
+    ASSERT_FLOAT_EQ(m_inv[3], expected_vals[3]);
+    ASSERT_FLOAT_EQ(m_inv[4], expected_vals[4]);
+    ASSERT_FLOAT_EQ(m_inv[5], expected_vals[5]);
+    ASSERT_FLOAT_EQ(m_inv[6], expected_vals[6]);
+    ASSERT_FLOAT_EQ(m_inv[7], expected_vals[7]);
+    ASSERT_FLOAT_EQ(m_inv[8], expected_vals[8]);
+}
+
+TEST(matrix3, matrix_times_invert_equals_identiy)
+{
+    float vals[] = {
+        1.f, 2.f, -1.f,
+        2.f, 1.f, 2.f,
+        -1.f, 2.f, 1.f};
+
+    const auto identity = ufps::Matrix3{};
+
+    const auto m = ufps::Matrix3{vals};
+
+    const auto m_inv = ufps::Matrix3::invert(m);
+
+    const auto result = m * m_inv;
+
+    ASSERT_FLOAT_EQ(result[0], identity[0]);
+    ASSERT_FLOAT_EQ(result[1], identity[1]);
+    ASSERT_FLOAT_EQ(result[2], identity[2]);
+    ASSERT_FLOAT_EQ(result[3], identity[3]);
+    ASSERT_FLOAT_EQ(result[4], identity[4]);
+    ASSERT_FLOAT_EQ(result[5], identity[5]);
+    ASSERT_FLOAT_EQ(result[6], identity[6]);
+    ASSERT_FLOAT_EQ(result[7], identity[7]);
+    ASSERT_FLOAT_EQ(result[8], identity[8]);
+}
