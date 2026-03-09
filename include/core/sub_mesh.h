4c71bb12a39fe06d1d6dd29bfde522134d023650
Christian Moyzyczyk <motzky@gmx.net>
2026-03-09 21:09:12 +0100

    Add SubMesh class

---
+++ include/core/sub_mesh.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cstdint>
+
+#include "graphics/mesh_manager.h"
+#include "graphics/mesh_view.h"
+#include "math/aabb.h"
+
+namespace ufps
+{
+    class SubMesh
+    {
+    public:
+        SubMesh(MeshView mesh_view, std::uint32_t material_index, const MeshManager &mesh_manager);
+
+        auto mesh_view() const -> MeshView;
+        auto material_index() const -> std::uint32_t;
+        auto aabb() const -> const AABB &;
+
+    private:
+        MeshView _mesh_view;
+        std::uint32_t _material_index;
+        AABB _aabb;
+    };
+
+}
