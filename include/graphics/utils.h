b40f5575085809e0f9150e479c3f73c4381d0899
Christian Moyzyczyk <motzky@gmx.net>
2026-03-18 21:15:40 +0100

    don't flip textures by default

--- include/graphics/utils.h
+++ include/graphics/utils.h
@@ -42,8 +42,8 @@ namespace ufps
         }
     }
 
-    auto load_texture(ResourceLoader &resource_loader, std::string id, bool flip = true) -> TextureData;
-    auto load_texture(DataBufferView image_data, bool flip = true) -> TextureData;
+    auto load_texture(ResourceLoader &resource_loader, std::string id, bool flip = false) -> TextureData;
+    auto load_texture(DataBufferView image_data, bool flip = false) -> TextureData;
 
     auto load_model(DataBufferView model_data, ResourceLoader &resource_loader, std::string format = "") -> std::tuple<std::string, std::vector<ModelData>>;
 
