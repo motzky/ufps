f71c05122562e61390684e7d9bcb2c2d4aa33502
Christian Moyzyczyk <motzky@gmx.net>
2026-04-19 23:38:21 +0200

    Fix threadpool test

--- src/concurrency/cond_var.cpp
+++ src/concurrency/cond_var.cpp
@@ -4,10 +4,12 @@ namespace ufps
 {
     auto CondVar::notify_one() -> void
     {
+        _cv.notify_one();
     }
 
     auto CondVar::notify_all() -> void
     {
+        _cv.notify_all();
     }
 
 }
