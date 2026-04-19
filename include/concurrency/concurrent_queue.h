95e6db402dfde63b575b2650eeccd090894b4581
Christian Moyzyczyk <motzky@gmx.net>
2026-04-19 23:35:38 +0200

    Add ConcurrentQueue

---
+++ include/concurrency/concurrent_queue.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <atomic>
+#include <cstdint>
+#include <queue>
+
+#include "concurrency/cond_var.h"
+#include "concurrency/lock.h"
+
+namespace ufps
+{
+
+    template <class T, class Q = std::queue<T>>
+    class ConcurrentQueue
+    {
+    public:
+        auto pop() -> T
+        {
+            const auto lock = std::scoped_lock{_lock};
+            --_size;
+
+            auto obj = std::move(_q.front());
+            _q.pop();
+
+            return obj;
+        }
+
+        auto push(T &&obj) -> void
+        {
+            const auto lock = std::scoped_lock{_lock};
+            ++_size;
+            _q.push(std::forward<T>(obj));
+        }
+
+        auto empty() const -> bool
+        {
+            return _size == 0u;
+        }
+
+        auto size() const -> std::uint32_t
+        {
+            return _size;
+        }
+
+    private:
+        Q _q;
+        Lock<> _lock;
+        CondVar _cv;
+        std::atomic<std::uint32_t> _size;
+    };
+
+}
