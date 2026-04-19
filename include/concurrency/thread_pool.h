a088aa3937441261cfc3187e8ccda0d835a6b510
Christian Moyzyczyk <motzky@gmx.net>
2026-04-19 23:36:23 +0200

    Add ThreadPool and test

---
+++ include/concurrency/thread_pool.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <atomic>
+#include <cstdint>
+#include <format>
+#include <functional>
+#include <vector>
+
+#include "concurrency/concurrent_queue.h"
+#include "concurrency/cond_var.h"
+#include "concurrency/thread.h"
+
+namespace ufps
+{
+    using Job = std::move_only_function<void()>;
+
+    class ThreadPool
+    {
+    public:
+        ThreadPool();
+        ThreadPool(std::uint32_t worker_count);
+        ~ThreadPool();
+
+        auto add(Job job) -> void;
+
+        auto worker_count() -> std::uint32_t;
+
+        auto drain() const -> void;
+
+    private:
+        auto worker(std::stop_token stop_token) -> void;
+
+        std::uint32_t _worker_count;
+        std::vector<Thread> _workers;
+        ConcurrentQueue<Job> _job_queue;
+        Lock<> _worker_lock;
+        CondVar _worker_cv;
+        std::atomic<std::uint32_t> _job_count;
+        std::atomic<bool> _jobs_complete;
+    };
+}
---
+++ src/concurrency/thread_pool.cpp
@@ -0,0 +1,102 @@
+#include "concurrency/thread_pool.h"
+
+#include <algorithm>
+#include <thread>
+
+#include "concurrency/concurrent_queue.h"
+#include "concurrency/cond_var.h"
+#include "concurrency/thread.h"
+#include "log.h"
+
+namespace ufps
+{
+    ThreadPool::ThreadPool()
+        : ThreadPool(std::clamp(std::thread::hardware_concurrency() - 1u, 1u, 32u))
+    {
+    }
+
+    ThreadPool::ThreadPool(std::uint32_t worker_count)
+        : _worker_count{worker_count},
+          _workers{},
+          _job_queue{},
+          _worker_cv{},
+          _job_count{},
+          _jobs_complete{true}
+    {
+        log::info("starting thread poll with {} workers", worker_count);
+
+        for (auto i = 0u; i < worker_count; ++i)
+        {
+            const auto name = std::format("worker_{}", i);
+
+            _workers.push_back({name, [this](std::stop_token stop_token)
+                                { worker(std::move(stop_token)); }});
+        }
+    }
+
+    ThreadPool::~ThreadPool()
+    {
+        for (auto &thread : _workers)
+        {
+            thread.request_stop();
+        }
+    }
+
+    auto ThreadPool::add(Job job) -> void
+    {
+        _job_queue.push(std::move(job));
+        ++_job_count;
+        _jobs_complete = false;
+        _worker_cv.notify_one();
+    }
+
+    auto ThreadPool::worker_count() -> std::uint32_t
+    {
+        return _worker_count;
+    }
+
+    auto ThreadPool::drain() const -> void
+    {
+        while (_job_count != 0u)
+        {
+            _jobs_complete.wait(true);
+        }
+    }
+
+    auto ThreadPool::worker(std::stop_token stop_token) -> void
+    {
+        log::info("starting worker thread: {}", std::this_thread::get_id());
+
+        while (!stop_token.stop_requested())
+        {
+            auto job = Job{};
+
+            {
+                auto lock = std::unique_lock{_worker_lock};
+                _worker_cv.wait(
+                    lock,
+                    stop_token,
+                    [&]
+                    { return !_job_queue.empty(); });
+
+                if (stop_token.stop_requested())
+                {
+                    break;
+                }
+
+                job = _job_queue.pop();
+            }
+
+            log::debug("doing work on worker thread: {}", std::this_thread::get_id());
+            job();
+            --_job_count;
+
+            if (_job_count == 0u)
+            {
+                _jobs_complete = true;
+                _jobs_complete.notify_one();
+            }
+        }
+        log::info("ending worker thread: {}", std::this_thread::get_id());
+    }
+}
