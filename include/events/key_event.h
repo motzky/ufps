be9438067a90b426fa06250bb0fa7a5a9a653666
Christian Moyzyczyk <motzky@gmx.net>
2026-04-28 23:34:46 +0200

    constexpr KeyEvent

--- include/events/key_event.h
+++ include/events/key_event.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <format>
+
 #include "events/key.h"
+#include <utils/formatter.h>
 
 namespace ufps
 {
@@ -8,15 +11,36 @@ namespace ufps
     {
 
     public:
-        KeyEvent(Key key, KeyState state);
-        auto key() const -> Key;
-        auto state() const -> KeyState;
+        constexpr KeyEvent(Key key, KeyState state);
+        constexpr auto key() const -> Key;
+        constexpr auto state() const -> KeyState;
 
-        auto operator==(const KeyEvent &) const -> bool = default;
-        auto to_string() const -> std::string;
+        constexpr auto operator==(const KeyEvent &) const -> bool = default;
+        constexpr auto to_string() const -> std::string;
 
     private:
         Key _key;
         KeyState _state;
     };
+
+    constexpr KeyEvent::KeyEvent(Key key, KeyState state)
+        : _key(key),
+          _state(state)
+    {
+    }
+
+    constexpr auto KeyEvent::key() const -> Key
+    {
+        return _key;
+    }
+
+    constexpr auto KeyEvent::state() const -> KeyState
+    {
+        return _state;
+    }
+
+    constexpr auto KeyEvent::to_string() const -> std::string
+    {
+        return std::format("KeyEvent {} {}", _key, _state);
+    }
 }
