2a21b7c4da975c2e63c5af1c7fda05114a3cd8d8
Christian Moyzyczyk <motzky@gmx.net>
2026-06-08 20:32:05 +0200

    Add PhysicsSystem

---
+++ src/physics/physics_system.cpp
@@ -0,0 +1,17 @@
+#include "physics/physics_system.h"
+
+namespace ufps
+{
+    PhysicsSystem::PhysicsSystem()
+    {
+    }
+
+    PhysicsSystem::~PhysicsSystem()
+    {
+    }
+
+    auto PhysicsSystem::update() -> void
+    {
+    }
+
+}
