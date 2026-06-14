71b3a25a3c11fce638097f2e6ea2e8a005a4b151
Christian Moyzyczyk <motzky@gmx.net>
2026-06-14 22:36:52 +0200

    Implement SimpleBroadPhaseLayer and collision filters for Jolt Physics integration

--- include/physics/utils.h
+++ include/physics/utils.h
@@ -5,50 +5,67 @@
 #include <string>
 #include <unordered_map>
 
-// #include <Jolt/Jolt.h>
-
-// #include <Jolt/Math/Vec3.h>
-
-// #include <Jolt/Physics/Collision/BroadPhase/BroadPhaseLayer.h>
-
+#include "physics/jolt.h"
 #include "physics/physics_layers.h"
 #include "utils/ensure.h"
+#include "utils/formatter.h"
 
 namespace ufps
 {
 
-    // class SimpleBroadPhaseLayer : public ::JPH::BroadPhaseLayerInterface
-    // {
-    // public:
-    //     virtual auto GetNumBroadPhaseLayers() const -> ::JPH::uint override
-    //     {
-    //         return std::ranges::size(std::meta::enumerators_of(^^PhysicsLayer));
-    //     }
+    class SimpleBroadPhaseLayer : public ::JPH::BroadPhaseLayerInterface
+    {
+    public:
+        auto GetNumBroadPhaseLayers() const -> ::JPH::uint override
+        {
+            return std::ranges::size(std::meta::enumerators_of(^^PhysicsLayer));
+        }
 
-    //     virtual auto GetBroadPhaseLayer(::JPH::ObjectLayer layer) const -> ::JPH::BroadPhaseLayer override
-    //     {
-    //         return ::JPH::BroadPhaseLayer{static_cast<::JPH::BroadPhaseLayer::Type>(layer)};
-    //     }
+        auto GetBroadPhaseLayer(::JPH::ObjectLayer layer) const -> ::JPH::BroadPhaseLayer override
+        {
+            return ::JPH::BroadPhaseLayer{static_cast<::JPH::BroadPhaseLayer::Type>(layer)};
+        }
 
-    //     virtual auto GetBroadPhaseLayerName(::JPH::BroadPhaseLayer layer) const -> const char * override
-    //     {
-    //         const auto native_layer = PhysicsLayer{layer.GetValue()};
+        auto GetBroadPhaseLayerName(::JPH::BroadPhaseLayer layer) const -> const char * override
+        {
+            const auto native_layer = PhysicsLayer{layer.GetValue()};
 
-    //         static auto lookup = []
-    //         {
-    //             auto result = std::unordered_map<PhysicsLayer, std::string>{};
-    //             template for (constexpr auto e : std::define_static_array(std::meta::enumerators_of(^^PhysicsLayer)))
-    //             {
-    //                 result[[:e:]] = std::string(std::meta::identifier_of(e));
-    //             }
-    //             return result;
-    //         }();
+            static auto lookup = []
+            {
+                auto result = std::unordered_map<PhysicsLayer, std::string>{};
+                template for (constexpr auto e : std::define_static_array(std::meta::enumerators_of(^^PhysicsLayer)))
+                {
+                    result[[:e:]] = std::string(std::meta::identifier_of(e));
+                }
+                return result;
+            }();
 
-    //         const auto find = lookup.find(native_layer);
+            const auto find = lookup.find(native_layer);
 
-    //         // ensure(find != std::ranges::cend(lookup), "could not find layer: {}", native_layer);
-    //         return find->second.c_str();
+            ensure(find != std::ranges::cend(lookup), "could not find layer: {}", native_layer);
+            return find->second.c_str();
+        }
 
-    // private:
-    // };
+    private:
+    };
+
+    class SimpleObjectVsBroadPhaseLayerFilter : public ::JPH::ObjectVsBroadPhaseLayerFilter
+    {
+    public:
+        auto ShouldCollide(::JPH::ObjectLayer layer1, ::JPH::BroadPhaseLayer layer2) const -> bool override
+        {
+            return PhysicsLayer{layer1} == PhysicsLayer::DYNAMIC ||
+                   PhysicsLayer{layer2.GetValue()} == PhysicsLayer::DYNAMIC;
+        }
+    };
+
+    class SimpleObjectLayerPairFilter : public ::JPH::ObjectLayerPairFilter
+    {
+    public:
+        auto ShouldCollide(::JPH::ObjectLayer layer1, ::JPH::ObjectLayer layer2) const -> bool override
+        {
+            return PhysicsLayer{layer1} == PhysicsLayer::DYNAMIC ||
+                   PhysicsLayer{layer2} == PhysicsLayer::DYNAMIC;
+        }
+    };
 }
