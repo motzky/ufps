92096c8dad7321788287123687ff53d7e712aaa6
Christian Moyzyczyk <motzky@gmx.net>
2026-07-08 22:05:21 +0200

    fix contract validation handler definitions

--- src/core/contract_violation_handler.cpp
+++ src/core/contract_violation_handler.cpp
@@ -3,10 +3,10 @@
 
 #include "log.h"
 
-auto handle_contract_violation(std::contracts::contract_violation cv) -> void;
+auto handle_contract_violation(const std::contracts::contract_violation &cv) -> void;
 
-auto handle_contract_violation(std::contracts::contract_violation cv) -> void
+auto handle_contract_violation(const std::contracts::contract_violation &cv) -> void
 {
     ufps::log::error("contract violation {}", cv.comment());
-    ufps::log::error("{}", std::stacktrace::current(3));
+    ufps::log::error("{}", std::stacktrace::current(2));
 }
