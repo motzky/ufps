3fe5e3bcb3728a0864e5e025d6f88d870e9497ca
Christian Moyzyczyk <motzky@gmx.net>
2026-07-12 17:58:03 +0200

    Also duplicate its rigid bodies when duplicating an entity

--- src/graphics/debug_renderer.cpp
+++ src/graphics/debug_renderer.cpp
@@ -310,6 +310,12 @@ namespace
                 auto *entity = *selected_entity;
                 auto *new_entity = value.scene.create_entity(entity->name());
                 new_entity->set_transform(entity->transform());
+
+                for (const auto handle : entity->rigid_bodies())
+                {
+                    new_entity->add_rigid_body(ufps::service<ufps::PhysicsSystem>().duplicate_rigid_body(handle));
+                }
+
                 *value.selected = new_entity;
             }
             if (auto *selected_light = std::get_if<ufps::PointLightHandle>(value.selected))
